0517ffa255e5b50cd3275e9186642311ad4586b6
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-13 16:25:25 -0300

    feat(CreditCardManagementPlatform) - Creating the CreditCardManagementPlatform file

---
+++ src/main/java/com/creditvault/creditcardmanagement/CreditCardManagementPlatforApplication.java
@@ -0,0 +1,13 @@
+package com.creditvault.creditcardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardManagementPlatforApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardManagementPlatforApplication.class, args);
+	}
+
+}
