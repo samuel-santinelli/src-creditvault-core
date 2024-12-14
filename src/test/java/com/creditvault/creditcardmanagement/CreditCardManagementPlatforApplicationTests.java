e5d0331f9dd2c21f49782359ec89d32b92071dc9
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-14 16:25:31 -0300

    feat(CreditCardManagementPlatform) - Creating the CreditCardManagementPlatform file

---
+++ src/test/java/com/creditvault/creditcardmanagement/CreditCardManagementPlatforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditvault.creditcardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardManagementPlatforApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
