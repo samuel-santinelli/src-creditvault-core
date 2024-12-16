1f7bf4d0bb628827a8cddedc8a201977b51c017e
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-16 16:46:47 -0300

    feat(CreditCardManagementPlatform) - Changing the class name

--- src/main/java/com/creditvault/creditcardmanagement/CreditCardManagementPlatformApplication.java
+++ src/main/java/com/creditvault/creditcardmanagement/CreditCardManagementPlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreditCardManagementPlatforApplication {
+public class CreditCardManagementPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreditCardManagementPlatforApplication.class, args);
+		SpringApplication.run(CreditCardManagementPlatformApplication.class, args);
 	}
 
 }
