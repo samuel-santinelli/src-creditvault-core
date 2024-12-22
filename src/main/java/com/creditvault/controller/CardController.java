09bef36ff2f639d1baea69a753abe5c401a6b148
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-22 15:02:27 -0300

    feat(CardController) - Creating the api methods in CardController

--- src/main/java/com/creditvault/controller/CardController.java
+++ src/main/java/com/creditvault/controller/CardController.java
@@ -1,4 +1,39 @@
 package com.creditvault.controller;
 
+import com.creditvault.model.Card;
+import com.creditvault.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/cards")
 public class CardController {
+
+    @Autowired
+    private CardService cardService;
+
+    // Endpoint for to save a card
+    @PostMapping
+    public ResponseEntity<Card> createCard(@RequestBody Card card) {
+        Card savedCard = cardService.saveCard(card);
+        return new ResponseEntity<>(savedCard, HttpStatus.CREATED);
+    }
+
+    // Endpoint for to search a card
+    @GetMapping("/{id}")
+    public ResponseEntity<Card> getCardById(@PathVariable Long id) {
+        Optional<Card> card = cardService.getCardById(id);
+        return card.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Endpoint for to search a card by number
+    @GetMapping("/number/{cardNumber}")
+    public ResponseEntity<Card> getCardByNumber(@PathVariable String cardNumber) {
+        Optional<Card> card = cardService.getCardByNumber(cardNumber);
+        return card.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
