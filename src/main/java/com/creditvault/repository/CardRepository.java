5c6714efaaa739ae78850f2408e8e221362179bf
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-24 15:03:43 -0300

    feat(Card) - Creating the interface and the first repository method in CardRepository file

--- src/main/java/com/creditvault/repository/CardRepository.java
+++ src/main/java/com/creditvault/repository/CardRepository.java
@@ -4,7 +4,12 @@ import com.creditvault.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CreditCardRepository extends JpaRepository<Card, Long> {
-    Card findByCardNumber(String cardNumber);
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+    // Método para buscar cartão pelo número
+    Optional<Card> findByCardNumber(String cardNumber);
+
 }
