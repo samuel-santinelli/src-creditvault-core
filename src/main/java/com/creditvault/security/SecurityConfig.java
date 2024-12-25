6c8e8d9db10b24219bddf7ed257bc74092424acc
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-25 15:05:14 -0300

    feat(SecurityConfig) - Creating the configure method desable CSRF for the tests

--- src/main/java/com/creditvault/security/SecurityConfig.java
+++ src/main/java/com/creditvault/security/SecurityConfig.java
@@ -1,4 +1,21 @@
-package com.creditvault.security;
+package com.creditvault.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()  // Desabilita CSRF para facilitar testes
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/cards").permitAll()  // Permite POST sem autenticação
+                .anyRequest().authenticated();  // Exige autenticação para qualquer outra requisição
+    }
 }
