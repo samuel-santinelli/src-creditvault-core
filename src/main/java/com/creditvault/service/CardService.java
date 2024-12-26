2d9a0f4a62e02d833b8824b87bb7aa74638c932c
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-26 15:05:51 -0300

    feat(SecurityConfig) - Creating the CardService method, and connecting with the repository

--- src/main/java/com/creditvault/service/CardService.java
+++ src/main/java/com/creditvault/service/CardService.java
@@ -1,4 +1,30 @@
 package com.creditvault.service;
 
+import com.creditvault.model.Card;
+import com.creditvault.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class CardService {
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    // Método para salvar o cartão
+    public Card saveCard(Card card) {
+        return cardRepository.save(card);
+    }
+
+    // Método para encontrar o cartão pelo ID
+    public Optional<Card> getCardById(Long id) {
+        return cardRepository.findById(id);
+    }
+
+    // Método para buscar um cartão pelo número
+    public Optional<Card> getCardByNumber(String cardNumber) {
+        return cardRepository.findByCardNumber(cardNumber);
+    }
 }
